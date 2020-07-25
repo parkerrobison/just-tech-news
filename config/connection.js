78f76a541bebf8b9a680d99734eac431b4417dbc
probison <parker.robison@hotmail.com>
2020-07-24 20:02:25 -0600

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -8,10 +8,16 @@ require('dotenv').config();
 // the process.env variables serve as a stand in for database name, username, and password.
 // this is done so private info like passwords aren't displayed publicly. NPM package dotenv does this.
 // the .env file contains "sensistive info" and then we list in gitignore to prevent it from being displayed.
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else{
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
 
 module.exports = sequelize;
