5293254c9c00e6d568cd0946e0b87daab923fa77
probison <parker.robison@hotmail.com>
2020-07-29 22:17:35 -0600

    added login/out functions and routes.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -18,6 +18,7 @@ const { Post, User, Comment } = require('../models');
 //   });
 
 router.get('/', (req, res) => {
+    console.log(req.session);
     Post.findAll({
         attributes: [
             'id',
@@ -43,7 +44,7 @@ router.get('/', (req, res) => {
     })
     .then(dbPostData => {
         //pass a single post object into the hompage template
-        console.log(dbPostData[0])
+        // console.log(dbPostData[0])
         const posts = dbPostData.map(post => post.get({ plain: true }));
         // this is a serialized method to get only the information that you need.
         // res.json does this automatically, and that is why you never used serialization before.
@@ -53,6 +54,15 @@ router.get('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -11,6 +11,23 @@ const PORT = process.env.PORT || 3001;
 const exphbs = require('express-handlebars')
 const hbs = exphbs.create({});
 
+// the following code sets up an Express.js session and connects the session to our sequelize database.
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -26,7 +43,6 @@ app.use(routes);
 // turn on connection to db and server
 // sync means that Sequelize is taking the models and connecting them to the associated db tables.
 // if they don't exist yet, it will create it.
-
 // force: true would drop and re-create all of the database tables on startup.
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
