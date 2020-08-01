3ecb7e38dfabfa44fe1582fcbe039b7721271321
probison <parker.robison@hotmail.com>
2020-07-31 20:08:44 -0600

    added partials and helper functions

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 
 // these following lines of code set up handlebars.js as a template engine.
 const exphbs = require('express-handlebars')
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 // the following code sets up an Express.js session and connects the session to our sequelize database.
 const session = require('express-session');
